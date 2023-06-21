2fb547b93c8c454a1226ef22abff66ad35e7e9b7
Adil Bendaoui <90851562+Abendaoui@users.noreply.github.com>
2023-06-21 01:46:21 +0100

    files

--- frontend/src/views/admin/pages/Customers.jsx
+++ frontend/src/views/admin/pages/Customers.jsx
@@ -68,7 +68,7 @@ const Customers = () => {
                         </svg>
                     </span>
                     <input
-                        placeholder="Search By Employee"
+                        placeholder="Search By Customer"
                         value={chosenCustomer}
                         onChange={(e) => setChosenCustomer(e.target.value)}
                         className="appearance-none rounded-r rounded-l sm:rounded-l-none border border-gray-200 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none"
